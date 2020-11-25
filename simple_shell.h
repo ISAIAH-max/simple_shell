9922f6798e197e8d76f05a319a403f06f639d8ae
ISAIAH-max <mlewaisaiah@gmail.com>
2020-11-25 15:53:34 +0300

    Header file

---
+++ simple_shell.h
@@ -0,0 +1,14 @@
+#ifndef SIMPLE_SHELL_H
+#define SIMPLE_SHELL_H
+
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <wait.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#endif /* SIMPLE_SHELL_H */
