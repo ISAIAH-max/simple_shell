b59f99b451c45a0f0e427910149d04ac19b501de
ISAIAH-max <mlewaisaiah@gmail.com>
2020-11-25 23:34:30 +0300

    Entry Point

---
+++ main.c
@@ -0,0 +1,44 @@
+#include "simple_shell.h"
+
+void prompt(void);
+
+/**
+ *main- A function that allow execution of commands
+ *Return: 1
+ */
+
+int main(void)
+{
+	int stat = 1, i;
+	char **args;
+	char *string;
+	char *argv[1024];
+	char *token;
+
+	while (stat)
+	{
+		prompt();
+		string = get_line();
+		token = strtok(string, " ");
+		i = 0;
+		while (token != NULL)
+		{
+			argv[i] = token;
+			token = strtok(NULL, " ");
+			i++;
+		}
+		argv[i] = NULL;
+		args = argv;
+		stat = exec_cmd(args);
+	}
+	return (0);
+}
+
+/**
+*prompt- A function that prints charcter "$ "
+*/
+
+void prompt(void)
+{
+	printf("$");
+}
